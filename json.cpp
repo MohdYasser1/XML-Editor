694aa17a2ad401bb35010f7b9b3ae9b427ad6549
MarlineMansour <131539534+MarlineMansour@users.noreply.github.com>
2023-12-28 11:01:17 +0200

    Update json.cpp

--- json.cpp
+++ json.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <regex>
 #include <sstream>
+
 using namespace std;
 
 class TagValuePair {
@@ -70,8 +71,13 @@ public:
         }
         return depth;
     }
+};
 
-
+class SString {
+public:
+    static vector<string> stringToList(string str, char sep = ' ', bool greedy = true);
+ 
+    static string insert_taps(int level);
 };
 
 string extractTagName(const string& line);
@@ -80,17 +86,16 @@ void printTree(const Node* node, int level);
 Node* parseXML(const std::string& xml);
 std::string jsonFormat(const Node* node, int level);
 
-
 Node* parseXML(const std::string& xml) {
     Node* root = nullptr;
     Node* currentNode = nullptr;
     string currentText;
     istringstream iss(xml);
 
-   string line;
+    string line;
     while (std::getline(iss, line)) {
         if (line.find('<') != std::string::npos) {
-          string tagName = extractTagName(line);
+            string tagName = extractTagName(line);
             if (tagName[0] == '/') {
                 currentNode->setTagValue(currentText);
                 currentNode = currentNode->getParent();
@@ -135,7 +140,7 @@ Node* parseXML(const std::string& xml) {
     return root;
 }
 
- string extractTagName(const std::string& line) {
+std::string extractTagName(const std::string& line) {
     std::regex pattern("<(.*?)>");
     std::smatch matcher;
     if (std::regex_search(line, matcher, pattern)) {
@@ -162,195 +167,135 @@ void printTree(const Node* node, int level) {
     }
 }
 
-
-string jsonFormat(const Node* node, int level) {
+string insert_taps(int level) {
+    string taps = "";
+    for (int i = 0; i < level; i++) {
+        taps += "\t";
+    }
+    return taps;
+}
+std::string jsonFormat(const Node* node, int level) {
     std::string result;
-    std::string indent(level, ' ');
 
     if (!node->getChildren().empty()) {
         bool hasSameTagName = false;
         for (size_t i = 1; i < node->getChildren().size(); ++i) {
             if (node->getChildren()[i]->getTagName() == node->getChildren()[0]->getTagName()) {
                 hasSameTagName = true;
-                //break;
+                break;
             }
         }
 
-        result += indent + "\"" + node->getTagName() + "\" : ";
+        result += insert_taps(level) + "\"" + node->getTagName() + "\" : ";
         if (hasSameTagName) {
             result += "[\n";
             for (size_t i = 0; i < node->getChildren().size(); ++i) {
-                if (i == 0) {
-                    //result += "\n";
-                    const Node* child = node->getChildren()[i];
-                 result +=indent+jsonFormat(child, level+1);
-                }
-                else{
-                result += "\n";
-                const Node* child = node->getChildren()[i];
-                 result +=indent+ "{\n";
-                 result += "\"" + child->getTagValue() + "\"";
-                  result += "\n"+ indent+"},\n";
-                }
-             
-            }
-            result += "\n" + indent + "]";
-        } else {
-            result += "{\n";
-            for (size_t i = 0; i < node->getChildren().size(); ++i) {
-               if (i != 0) {
-                   result += "\n";
+                if (i != 0) {
+                    result += "\n";
                 }
 
                 const Node* child = node->getChildren()[i];
-                result +=jsonFormat(child, level + 1);
+                result += jsonFormat(child, level + 1);
             }
-            result += "\n" + indent + "}";
+            result += "\n" + insert_taps(level) + "]";
+        } else {
+            result += "{\n";
+            for (size_t i = 0; i < node->getChildren().size(); ++i) {
+                if (i != 0) {
+                    result += "\n";
+                }
+
+                const Node* child = node->getChildren()[i];
+                result += jsonFormat(child, level + 1);
+            }
+            result += "\n" + insert_taps(level) + "}";
         }
     } else {
-        result += indent + "\"" + node->getTagName() + "\" : ";
-        result += "\"" + node->getTagValue() + "\",  ";
-       
+        result += insert_taps(level) + "\"" + node->getTagName() + "\" : ";
+        result += "\"" + node->getTagValue() + "\" , ";
     }
 
     return result;
 }
-
-
-
 //for testing
 /*int main() {
-    // Sample XML string
-    std::string xml = R"(<users>
-<user>
-<id>1</id>
-<name>Ahmed Ali</name>
-<posts>
-<post>
-<body> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </body>
-<topics>
-<topic> economy </topic>
-<topic> finance </topic>
-</topics>
-</post>
-<post>
-<body> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </body>
-<topics>
-<topic> solar_energy </topic>
-</topics>
-</post>
-</posts>
-<followers>
-<follower>
-<id>2</id>
-</follower>
-<follower>
-<id>3</id>
-</follower>
-</followers>
-</user>
-<user>
-<id>2</id>
-<name>Yasser Ahmed</name>
-<posts>
-<post>
-<body> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </body>
-<topics>
-<topic> education </topic>
-</topics>
-</post>
-</posts>
-<followers>
-<follower>
-<id>1</id>
-</follower>
-</followers>
-</user>
-<user>
-<id>3</id>
-<name>Mohamed Sherif</name>
-<posts>
-<post>
-<body> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </body>
-<topics>
-<topic> sports </topic>
-</topics>
-</post>
-</posts>
-<followers>
-<follower>
-<id>1</id>
-</follower>
-</followers>
-</user>
-</users>)";
+    // Example XML data
+    string xmlData = R"(
+        <users>
+            <user>
+                <id>1</id>
+                <name>Ahmed Ali</name>
+                <posts>
+                    <post>
+                        <body> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </body>
+                        <topics>
+                            <topic> economy </topic>
+                            <topic> finance </topic>
+                        </topics>
+                    </post>
+                    <post>
+                        <body> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </body>
+                        <topics>
+                            <topic> solar_energy </topic>
+                        </topics>
+                    </post>
+                </posts>
+                <followers>
+                    <follower>
+                        <id>2</id>
+                    </follower>
+                    <follower>
+                        <id>3</id>
+                    </follower>
+                </followers>
+            </user>
+            <user>
+                <id>2</id>
+                <name>Yasser Ahmed</name>
+                <posts>
+                    <post>
+                        <body> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </body>
+                        <topics>
+                            <topic> education </topic>
+                        </topics>
+                    </post>
+                </posts>
+                <followers>
+                    <follower>
+                        <id>1</id>
+                    </follower>
+                </followers>
+            </user>
+            <user>
+                <id>3</id>
+                <name>Mohamed Sherif</name>
+                <posts>
+                    <post>
+                        <body> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </body>
+                        <topics>
+                            <topic> sports </topic>
+                        </topics>
+                    </post>
+                </posts>
+                <followers>
+                    <follower>
+                        <id>1</id>
+                    </follower>
+                </followers>
+            </user>
+        </users>
+    )";
 
-    // Parse XML into a tree structure
-    Node* root = parseXML(xml);
-   string jsonTree = jsonFormat(root, 0);
+    // Parse XML
+    Node* root = parseXML(xmlData);
 
-   cout<< jsonTree<<endl;
+    // Convert XML to JSON
+    string jsonOutput = jsonFormat(root, 0);
 
-    delete root;
+    // Output JSON to console
+    cout << jsonOutput << endl;
+
+    // Clean up memory (Implement a function to delete the XML tree nodes)
+
     return 0;
-}*/
-
-/*std::stringjsonFormat(const Node node, int level) {
-    std::string result;
-    std::string indent(level, '   ');
-
-    if (!node->getChildren().empty()) {
-        bool hasSameTagName = false;
-        for (size_t i = 1; i < node->getChildren().size(); ++i) {
-            if (node->getChildren()[i]->getTagName() == node->getChildren()[0]->getTagName()) {
-                hasSameTagName = true;
-                break;
-            }
-        }
-
-        result += indent + "\"" + node->getTagName() + "\" : ";
-        if (hasSameTagName) {
-            result += "[\n";
-            for (size_t i = 0; i < node->getChildren().size(); ++i) {
-                if (i != 0) {
-                    result += "\n";
-                }
-
-                const Node* child = node->getChildren()[i];
-                result += jsonFormat(child, level + 1);
-            }
-            result += "\n" + indent + "]";
-        } else {
-            result += "{\n";
-            for (size_t i = 0; i < node->getChildren().size(); ++i) {
-                if (i != 0) {
-                    result += "\n";
-                }
-
-                const Node* child = node->getChildren()[i];
-                result += jsonFormat(child, level + 1);
-            }
-            result += "\n" + indent + "}";
-        }
-    } else {
-        result += indent + "\"" + node->getTagName() + "\" : ";
-
-        // Check if the tag value can be converted to a number
-        bool isNumber = true;
-        for (char c : node->getTagValue()) {
-            if (!std::isdigit(c)) {
-                isNumber = false;
-                break;
-            }
-        }
-
-        if (isNumber) {
-            result += "\"" + node->getTagValue() + "\"";
-        } else {
-            result += "\"" + node->getTagValue() + "\"";
-        }
-        result += ",";
-    }
-
-    return result;
 }*/
