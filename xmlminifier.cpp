5123d3432427f9675280a67125950f346b7007b5
Carol-Botros <130776634+Carol-Botros@users.noreply.github.com>
2023-12-29 23:38:05 +0200

    Update xmlminifier.cpp

--- xmlminifier.cpp
+++ xmlminifier.cpp
@@ -5,6 +5,6 @@ using namespace std;
 
 string xmlMinifier(const string& xml_string) {
     string minified_xml = xml_string;
-    minified_xml.erase(remove_if(minified_xml.begin(), minified_xml.end(), ::isspace), minified_xml.end());
+    minified_xml.erase(remove_if(minified_xml.begin(), minified_xml.end(), [](char c) { return ::isspace(c) && c != '\n'; }), minified_xml.end());
     return minified_xml;
 }
