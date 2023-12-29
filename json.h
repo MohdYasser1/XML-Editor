da7d7b3caaa755ee83f7b195bf0620268641508c
MarlineMansour <131539534+MarlineMansour@users.noreply.github.com>
2023-12-29 11:59:24 +0200

    Update json.h

--- json.h
+++ json.h
@@ -1,1 +1,7 @@
 #include "parsingXml.h"
+class SString {
+public:
+    static vector<string> stringToList(string str, char sep = ' ', bool greedy = true);
+ 
+    static string insert_taps(int level);
+};
