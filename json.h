e273a208c86e4765d0ad527e43c5be5d290fc8df
MarlineMansour <131539534+MarlineMansour@users.noreply.github.com>
2023-12-29 12:01:26 +0200

    Update json.h

--- json.h
+++ json.h
@@ -1,7 +1,9 @@
 #include "parsingXml.h"
+using namespace std;
 class SString {
 public:
     static vector<string> stringToList(string str, char sep = ' ', bool greedy = true);
  
     static string insert_taps(int level);
 };
+string jsonFormat(const Node* node, int level);
