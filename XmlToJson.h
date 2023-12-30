70109ee0ad77cd043ffe1343817001b14130356e
MarlineMansour <131539534+MarlineMansour@users.noreply.github.com>
2023-12-30 12:39:15 +0200

    Update XmlToJson.h

--- XmlToJson.h
+++ XmlToJson.h
@@ -1,12 +1,5 @@
 #include "parsingXml.cpp"
 using namespace std;
-class SString {
-public:
-    static vector<string> stringToList(string str, char sep = ' ', bool greedy = true);
- 
-    static string insert_taps(int level);
-};
 string jsonFormat(const Node* node, int level);
-string insert_taps(int level);
 string XML_2_Json(string XMLcontent);
 string print_json(const Node* node, int level );
